dd675b5ef6bc880145f3863060e675941b8b4d3c
Chris van den Berg <chrizus@gmail.com>
2024-06-25 11:12:31 +0200

    Update kingsat.py

--- kingofsat/spiders/kingsat.py
+++ kingofsat/spiders/kingsat.py
@@ -4,20 +4,31 @@ import scrapy
 class KingsatSpider(scrapy.Spider):
     
     name = 'kingofsat' #determine name of bot
-    allowed_domains = ['https://tr.kingofsat.net/tvsat-turksat4a.php','https://tr.kingofsat.net/tvsat-turksat3a.php'] #make sure you allowed urls
+    allowed_domains = ['https://en.kingofsat.net/sat-astra1l.php',
+                       'https://en.kingofsat.net/sat-astra1m.php',
+                       'https://en.kingofsat.net/sat-astra1n.php',
+                       'https://en.kingofsat.net/sat-astra3b.php',
+                       'https://en.kingofsat.net/sat-astra3c.php',
+                      ] #make sure you allowed urls
 
 
     def start_requests(self): #function for urls
         urls = (
-            (self.parse, 'https://tr.kingofsat.net/tvsat-turksat4a.php'),
-            (self.parse, 'https://tr.kingofsat.net/tvsat-turksat3a.php'),
+            (self.parse, 'https://en.kingofsat.net/sat-astra1l.php', 'Astra 1L'),
+            (self.parse, 'https://en.kingofsat.net/sat-astra1m.php', 'Astra 1M'),
+            (self.parse, 'https://en.kingofsat.net/sat-astra1n.php', 'Astra 1N'),
+            (self.parse, 'https://en.kingofsat.net/sat-astra3b.php', 'Astra 3B'),
+            (self.parse, 'https://en.kingofsat.net/sat-astra3c.php', 'Astra 3C'),
         )
         for callbackfunc, url in urls: #callback function for access each url in urls
             yield scrapy.Request(url, callback=callbackfunc)
 
 
     def parse(self, response):
+        '''
+        TODO: include satellite name in result table
 
+        '''
         tables=response.xpath('//*[@class="fl"]') #Path of every tables
         bases=response.xpath('//table[@class="frq"]/tr') #Path of ever tables's header
         """
@@ -38,10 +49,10 @@ class KingsatSpider(scrapy.Spider):
                 if row.xpath('.//td[3]/a/text()').extract_first() is not None and \
                     row.xpath('.//td[10]/text()[1]').extract_first() !='Audio':
                     yield {
-                        'Kanal' :       row.xpath('.//td[3]/a/text()').extract_first(),
-                        'Frekans':      base.xpath('.//td[3]/text()').extract_first(),
-                        'Polarizasyon': base.xpath('.//td[4]/text()').extract_first(),
-                        'Kapsam':       base.xpath('.//td[6]/a/text()').extract_first(),
+                        'Channel' :       row.xpath('.//td[3]/a/text()').extract_first(),
+                        'Frequency':      base.xpath('.//td[3]/text()').extract_first(),
+                        'Polarization': base.xpath('.//td[4]/text()').extract_first(),
+                        'Area':       base.xpath('.//td[6]/a/text()').extract_first(),
                         'SR':           base.xpath('.//td[9]/a[1]/text()').extract_first(),
                         'FEC':          base.xpath('.//td[9]/a[2]/text()').extract_first(),
                         'V-PID' :       row.xpath('.//td[9]/text()[1]').extract_first(),
